c3abe23d376232167542f1ba39e3a900986924eb
Mark Nunes <marknunes@Marks-iMac.local>
2020-10-12 14:37:33 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Source - [React With Typescript](https://www.youtube.com/watch?v=e5c_RfYqIhU).
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+const App: FC<AppProps> = ({sendSearchQuery}) => {
+  const [searchValue, setSearchValue] = useState<string>("")
+  const onSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = event.target.value
+  }
+
+  const search = () => {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={searchValue} onChange={onSearchInput} name="search" type="text" />
+      <button onClick={search}>Search</button>
     </div>
   );
 }
